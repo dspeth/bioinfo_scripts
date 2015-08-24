efec02bb7b099ac50da70fc628fb2d75d1eb8b1a
Daan Speth <d.speth@science.ru.nl>
2015-08-24 12:30:52 +0200

    Create prot_motif_search.pl

---
+++ proteins/prot_motif_search.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# a script to get a fasta file of reads that have blast hits against a custom database
+# requires:
+
+if (@ARGV != 3){
+	die "requires 3 arguments:\n\n1) protein fasta file\n2) user-defined motif\n3) user-defined outfile\n\n";
+}
+
+my $protein_fasta = $ARGV[0];
+my $motif = uc $ARGV[1];
+my $out_fasta = $ARGV[2]; 
+
+if (@ARGV != 3){ die "requires 3 arguments:\n\nprotein fasta file\nuser-defined motif\nuser-defined outfile" }
+ 
+
+
+#read fasta into hash
+my %prot;
+my $prot_id;
+
+open PROT, $protein_fasta or die "no read file provided";
+while (my $line = <PROT>){
+	chomp $line;
+	my $fc = substr($line, 0, 1);
+	if ($fc eq ">"){
+		$prot_id = substr($line,1);
+	}
+	else{
+		$prot{$prot_id} .= uc $line;
+	}
+}
+close PROT;
+
+# print out seqs that match input motif
+
+foreach $prot_id (keys %prot){
+	if ($prot{$prot_id} =~ /$motif/){
+		close OUT;
+    open OUT, ">> $out_fasta";
+    print OUT ">$prot_id\n$prot{$prot_id}\n";
+	}
+}
+
